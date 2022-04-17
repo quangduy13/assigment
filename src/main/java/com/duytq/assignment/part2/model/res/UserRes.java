20d518052deb51635bce0c7f5145bf6e75bb6546
duytq7 <duytq7@onemount.com>
2022-04-17 20:55:29 +0700

    update model

--- src/main/java/com/duytq/assignment/part2/model/res/UserRes.java
+++ src/main/java/com/duytq/assignment/part2/model/res/UserRes.java
@@ -1,2 +1,25 @@
-package com.duytq.assignment.part2.model.res;public class UserRes {
+package com.duytq.assignment.part2.model.res;
+
+import com.duytq.assignment.part2.model.User;
+
+public class UserRes {
+    private String message;
+
+    private User user;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
